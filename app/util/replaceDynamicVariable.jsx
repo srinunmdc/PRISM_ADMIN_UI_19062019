114e51859cd1cceb032e723c66c5d974a1615e4a
Faiz <ff250069@ncr.com>
2019-07-22 11:48:46 +0530

    testing

--- app/util/replaceDynamicVariable.jsx
+++ app/util/replaceDynamicVariable.jsx
@@ -1,22 +1,3 @@
-// const replaceDynamicVariable = (data, dynamicData) => {
-//   if (!dynamicData) {
-//     return data;
-//   }
-//   const regex = /\${\w+\}/g;
-//   let match = "";
-//   // eslint-disable-next-line no-cond-assign
-//   while ((match = regex.exec(data))) {
-//     let matchedString = data.substring(match.index + 2, regex.lastIndex - 1);
-//     debugger
-//     if (dynamicData[matchedString]) {
-//       data = data.replace(
-//         data.substring(match.index, regex.lastIndex),
-//         dynamicData[matchedString]
-//       );
-//     }
-//   }
-//   return data;
-// };
 
 const replaceDynamicVariable = (data, dynamicData) => {
   if (!dynamicData) {
